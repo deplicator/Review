db99a13598aa06df510b6ec64a232930652b9195
James Pryor <james@geekwagon.net>
2013-09-23 17:43:21 -0500

    Fixed bogosort, tweaked outputs.

--- sort_functions.cpp
+++ sort_functions.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
-#include <cstdlib>
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 
 using namespace std;
 
@@ -15,12 +16,16 @@ int main() {
     int Asize = 4;
     
     createArray(A, Asize);
-    cout << endl << "Unsorted array = ";
+    cout << endl << "  Unsorted array = ";
     printArray(A, Asize);
     cout << endl;
     cout << endl;
-    //bubbleSort(A, Asize);
+    
+    bubbleSort(A, Asize);
+    
+    randomizeArray(A, Asize);
     bogoSort(A, Asize);
+
 
 
     return 0;
@@ -51,52 +56,79 @@ void printArray(int array[], int size) {
     cout << "}";
 }
 
-
+/*
+ * Bubble sort.
+ */
 void bubbleSort(int array[], int size) {
-    cout << "Start Bubble Sort" << endl;
+
+    cout << "  Bubble Sort" << endl;
+    cout << "  -----------" << endl << endl;
+    cout << "  original array: ";
+    printArray(array, size);
+    cout << endl << endl;
+    
     for (int i = size; i > 1; i--) {
-        cout << "pass " << size - i + 1 << " = ";
+        cout << "  pass " << size - i + 1 << " = ";
         printArray(array, i);
         cout << endl;
         for (int j = 0; j < (i - 1); j++) {
-            cout << "    check " << j + 1 << " = " << array[j] << " against " << array[j + 1] << endl;
+            cout << "    check array[" << j << "] against array[" << j + 1 << "]" << endl;
             if (array[j] > array[j + 1]) {
-                cout << "        " << array[j] << " is higher than " << array[j + 1] << ", so swap." << endl;
+                cout << "    " << array[j] << " is higher than " << array[j + 1] << ", so swap." << endl;
                 int temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
-                cout << "        current array -->";
+                cout << "    current array -->";
                 printArray(array, i);
                 cout << endl << endl;
             } else {
-                cout << "        " << array[j] << " is lower than " << array[j + 1] << ", no change." << endl << endl;
+                cout << "    " << array[j] << " is lower than " << array[j + 1] << ", no change." << endl << endl;
             }
         }
-        
     }
+    
+    cout << "  final array: ";
+    printArray(array, size);
+    cout << endl << endl;
 }
 
 
+/*
+ * Bogosort
+ * http://en.wikipedia.org/wiki/Bogosort
+ */
 void bogoSort(int array[], int size) {
-    cout << "Start Bogosort on ";
+
+    cout << endl << "  Bogosort";
+    cout << endl << "  --------" << endl << endl;
+    cout << "    Initial array = ";
     printArray(array, size);
     cout << endl;
     int count = 0;
-    int globalCount = 1;
-    while(count < size) {
-        cout << "pass " << globalCount << endl;
-        if (array[count] < array[count + 1]) {
-            cout << "    check " << count + 1 << " - " << array[count] << " is lower than " << array[count + 1] << " continue."<< endl;
-            count ++;
+    int reshuffleCount = 0;
+    while(count < size-1) {
+        cout << "    checking element " << count << ": " << array[count];
+
+        if (array[count] < array[count + 1] || array[count] == array[count + 1] ) {
+            cout << " is lower than " << array[count + 1] << " continue."<< endl;
+            count++;
         } else {
             count = 0;
-            cout << "    check " << count + 1 << " - " << array[count] << " is not lower than " << array[count + 1] << endl << "Reshuffling array = ";
+            cout << " is not lower than " << array[count + 2] << endl << endl;
+            cout << "    Reshuffling array = ";
             randomizeArray(array, size);
             printArray(array, size);
+            reshuffleCount++;
             cout << endl;
         }
-        globalCount++;        
     }
+    cout << endl << "  It took " << reshuffleCount << " reshuffels to complete." << endl;
+    cout << "  final array: ";
+    printArray(array, size);
+    cout << endl << endl;
+    
+    
+
 }
 
 
